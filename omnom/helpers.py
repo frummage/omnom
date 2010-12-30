c193388134da76840b4303bed0297cb3cd68b5e8
Ed Page <eopage@byu.net>
2010-12-29 22:23:31 -0600

    [pylint] equality for numbers

--- omnom/helpers.py
+++ omnom/helpers.py
@@ -16,7 +16,7 @@ def load_image(name, colorkey=None):
 		raise SystemExit, message
 	image = image.convert()
 	if colorkey is not None:
-		if colorkey is -1:
+		if colorkey == -1:
 			colorkey = image.get_at((0, 0))
 		image.set_colorkey(colorkey, pygame.locals.RLEACCEL)
 	return image, image.get_rect()
